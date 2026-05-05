8dccc7a5fe53b447a49aa63acae03e047130ce56
eliassfarrrr <shadowgame13579@gmail.com>
2026-05-05 19:48:01 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// ✅ 1단계에서 복사한 Firebase 설정값으로 교체하세요
+const firebaseConfig = {
+  apiKey: "여기에_apiKey_입력",
+  authDomain: "여기에_authDomain_입력",
+  projectId: "여기에_projectId_입력",
+  storageBucket: "여기에_storageBucket_입력",
+  messagingSenderId: "여기에_messagingSenderId_입력",
+  appId: "여기에_appId_입력"
+};
